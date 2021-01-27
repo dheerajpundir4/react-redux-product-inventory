68b1e687f9ac9099d7630e41fee27dc6ebcad24d
dheeeraj54321 <dheerajpundir@gmail.com>
2021-01-27 20:10:35 +0530

    using Navbar, Nab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect, Switch, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+// Using component
 import AllProductPage from './components/displayProduct/AllProductPage';
 import AboutPage from './components/AboutPage';
 import AddProductPage from './components/curd/AddProductPage';
@@ -10,43 +12,51 @@ import DeleteProductPage from './components/curd/DeleteProductPage';
 import EditProductPage from './components/curd/EditProductPage';
 
 
+// Using bootstrap
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-class Links extends React.Component{
-    render(){
-      return (
-        <nav>          
-            <NavLink activeStyle={{fontWeight:"bold", color:"black" }} exact activeClassName="current" to="/">About</NavLink>&nbsp;&nbsp;&nbsp;
-       
-            <NavLink activeStyle={{fontWeight:"bold", color:"black"}}  activeClassName="current" to="/products">Products</NavLink>&nbsp;&nbsp;&nbsp;
-            <NavLink activeStyle={{fontWeight:"bold", color:"black" }} exact activeClassName="current" to="/Login">Login</NavLink>&nbsp;&nbsp;&nbsp;
-            <NavLink activeStyle={{fontWeight:"bold", color:"black" }} exact activeClassName="current" to="/Signup">Signup</NavLink>&nbsp;&nbsp;&nbsp;
-         </nav> 
-      );
-    }
+
+
+class Links extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar bg="light" expand="lg">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/products">Products</Nav.Link>
+            <Nav.Link href="/Login">Login</Nav.Link>
+            <Nav.Link href="/Signup">SignUp</Nav.Link>
+          </Nav>
+        </Navbar>
+      </>
+    );
+  }
 }
 
-export default class App extends React.Component{
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
-       <Router>
-         <div>
-            <Links/>
-            <Switch>
+      <Router>
+        <div>
+          <Links />
+          <Switch>
             <Route exact path="/products">
-                <AllProductPage/>
-              </Route>
-              <Route path="/addProduct" component={AddProductPage} />
-              <Route path="/view/:id" component={DisplayProductDetail} />
-              <Route path="/delete/:id" component={DeleteProductPage} />
-              <Route path="/edit/:id" component={EditProductPage} />
-              <Route exact path="/">
-                <AboutPage/>
-              </Route>
-              <Route path="/Login" component={Login} />
-              <Route path="/Signup" component={Signup} />
-            </Switch>
-          </div>
-      </Router> 
+              <AllProductPage />
+            </Route>
+            <Route path="/addProduct" component={AddProductPage} />
+            <Route path="/view/:id" component={DisplayProductDetail} />
+            <Route path="/delete/:id" component={DeleteProductPage} />
+            <Route path="/edit/:id" component={EditProductPage} />
+            <Route exact path="/">
+              <AboutPage />
+            </Route>
+            <Route path="/Login" component={Login} />
+            <Route path="/Signup" component={Signup} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
