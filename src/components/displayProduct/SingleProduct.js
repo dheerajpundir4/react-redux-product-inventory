8a1963c70af63f2310a90524fe3a7cded74cf2d1
dheeeraj54321 <dheerajpundir@gmail.com>
2021-01-27 20:46:57 +0530

    using buttion inside the link

--- src/components/displayProduct/SingleProduct.js
+++ src/components/displayProduct/SingleProduct.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+
+//using bootstrap
+import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 
 
-const SingleProduct=({id, productName,quantity,price})=>(
-  
 
-    <tr>
-     
-   
+const SingleProduct = ({ id, productName, quantity, price }) => (
+
+
+  <tr>
+
+
     <td> {productName}</td>
     <td> {quantity}</td>
-    <td> {price}</td>   
+    <td> {price}</td>
     <td>
-      <Link to={"/view/"+id} style={{ textDecoration: 'none'}}>View</Link>
-      <Link to={"/delete/"+id} style={{ textDecoration: 'none'}}>Delete</Link>
-      <Link to={"/edit/"+id} style={{ textDecoration: 'none'}}>Edit</Link>     
-        
+   
+      <Link to={"/view/" + id} style={{ textDecoration: 'none' }}>
+        <Button variant="secondary" size="sm">View</Button>
+      </Link>
+      &nbsp;&nbsp;
+      <Link to={"/delete/" + id} style={{ textDecoration: 'none' }}>
+        <Button variant="secondary" size="sm"> Delete</Button>
+      </Link>
+      &nbsp;&nbsp;
+      <Link to={"/edit/" + id} style={{ textDecoration: 'none' }}>
+        <Button variant="secondary" size="sm">Edit</Button>
+      </Link>
+
     </td>
   </tr>
- 
-  
+
+
 
 );
 
