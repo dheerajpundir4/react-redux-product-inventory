12b8715c110c285c56e4b3704df17b42dd263dd5
dheeeraj54321 <dheerajpundir@gmail.com>
2021-01-27 22:46:25 +0530

    XXX

--- src/components/oper/EditProductForm.js
+++ src/components/oper/EditProductForm.js
@@ -6,70 +6,70 @@ import Form from 'react-bootstrap/Form';
 
 
 export default function EditProductForm(props) {
-    
+
     const { handleSubmit, handleChange, values, errors } = useFormik(
         {
-       
-            initialValues: {   
-                id:props.product.id,         
+
+            initialValues: {
+                id: props.product.id,
                 productName: props.product.productName,
-                quantity:props.product.quantity,
+                quantity: props.product.quantity,
                 price: props.product.price
-              //price:1000
+                //price:1000
             },
             validate,
             onSubmit(values) {
-              console.log("dheeraj")
+                console.log("dheeraj")
                 console.log(props.product.id);
-                props.onEdit(values);          
-               
+                props.onEdit(values);
+
             }
         }
     );
 
 
-function validate(values) {
-   
-    const errors = {};
-    if (!values.productName) {
-        errors.productName = "ProductSingleRow Name is required";
+    function validate(values) {
+
+        const errors = {};
+        if (!values.productName) {
+            errors.productName = "ProductSingleRow Name is required";
+        }
+        if (!values.quantity) {
+            errors.quantity = "Quantity is required";
+        }
+        if (!values.price) {
+            errors.price = "Price is required";
+        }
+        if (values.price) {
+            values.price = "Rs. " + values.price
+        }
+        return errors
     }
-    if (!values.quantity) {
-        errors.quantity = "Quantity is required";
-    }
-    if (!values.price) {
-        errors.price = "Price is required";
-    }
-    if(values.price){       
-        values.price="Rs. "+values.price
-    }
-    return errors
-}
 
 
 
 
     return (
         <Form onSubmit={handleSubmit}>
-           
-           <Form.Group>
-           <input type="text" placeholder={props.product.productName} name="productName" onChange={handleChange} values={values.productName} />
-                {errors.productName?errors.productName:null}  
-           </Form.Group>
 
-           <Form.Group>
-           <input type="number" placeholder={props.product.quantity} name="quantity" onChange={handleChange} values={values.quantity}/>
-                {errors.quantity?errors.quantity:null}
-           </Form.Group>
-           <Form.Group>
-           <input type="number" placeholder={props.product.price} name="price" onChange={handleChange} values={values.price} />
-                {errors.price?errors.price:null}
-           </Form.Group>
+            <Form.Group>
+                <input type="text" placeholder={props.product.productName} name="productName" onChange={handleChange} values={values.productName} />
+                {errors.productName ? errors.productName : null}
+            </Form.Group>
 
-           <Form.Group>
-           <button type="submit">Submit</button>
-           </Form.Group>
-            
+            <Form.Group>
+                <input type="number" placeholder={props.product.quantity} name="quantity" onChange={handleChange} values={values.quantity} />
+                {errors.quantity ? errors.quantity : null}
+            </Form.Group>
+            <Form.Group>
+                <input type="number" placeholder={props.product.price} name="price" onChange={handleChange} values={values.price} />
+                {errors.price ? errors.price : null}
+            </Form.Group>
+
+            <Form.Group>
+                <button type="submit">Submit</button>
+            </Form.Group>
+
         </Form>
     )
 
