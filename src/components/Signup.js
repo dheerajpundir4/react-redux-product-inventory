2d0779ee52899ec836f6172897bc712c72a9bfa8
dheeeraj54321 <dheerajpundir@gmail.com>
2021-01-27 20:27:22 +0530

    using bootstrap form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-function Signup(){
-    return (
-        <>
-         <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <input type="password" placeholder="password again" />
-        <button>Sign Up</button>
-        <Link to="/login">Already have an account?</Link>
-      </form>
-        </>
-    )
+// Using bootstrap
+import Form from 'react-bootstrap/Form';
+
+function Signup() {
+  return (
+    <>
+      <Form>
+
+        <Form.Group>
+          <input type="email" placeholder="email" />
+        </Form.Group>
+
+        <Form.Group>
+          <input type="password" placeholder="password" />
+        </Form.Group>
+
+        <Form.Group>
+          <input type="password" placeholder="password again" />
+        </Form.Group>
+
+        <Form.Group>
+          <button>Sign Up</button>
+          <Link to="/login">Already have an account?</Link>
+        </Form.Group>
+
+      </Form>
+    </>
+  )
 }
 
 export default Signup;
