3c9f38b2feaec0a31c37b16af2943c7f4e124242
dheeeraj54321 <dheerajpundir@gmail.com>
2021-02-03 23:04:28 +0530

    most of the part is done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// Using component
-import AllProductPage from './pages/AllProduct/AllProductListPage';
-import AboutPage from './pages/AboutPage';
-
-import AddProductPage from './pages/AddProductPage';
-import ViewProductPage from './pages/ViewProductPage';
-import TopViewProduct from './pages/TopViewProduct'
-import Login from './user/Login';
-import Signup from './user/Signup';
-import Logout from './user/Logout';
-import DeleteProductPage from './opertion/DeleteProductForm';
-import EditProductPage from './pages/EditProductPage';
 import NavComponent from './components/NavComponent'
+import RouterComponent from './components/RouterComponent'
+import { Card, Container } from 'react-bootstrap';
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-
-        <Router>
-          <NavComponent />
-          <Switch>
-
-            <Route path="/products" component={AllProductPage}/> 
-            <Route path="/about" component={AboutPage}/> 
-            <Route path="/topViewProduct" component={TopViewProduct} />
-            <Route path="/addProduct" component={AddProductPage} />
-            <Route path="/view/:id" component={ViewProductPage} />
-            <Route path="/delete/:id" component={DeleteProductPage} />
-            <Route path="/edit/:id" component={EditProductPage} />
-            <Route exact path="/">
-              <AllProductPage />
-            </Route>
-            <Route path="/Login" component={Login} />
-            <Route path="/Signup" component={Signup} />
-            <Route path="/Logout" component={Logout} />
-          </Switch>
-
-        </Router>
-      </div>
+      <Container fluid="xl">
+        <Card>
+      <NavComponent />
+      <RouterComponent />
+       </Card>
+      </Container>
     );
   }
 }
--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -6,27 +6,30 @@ import Nav from 'react-bootstrap/Nav';
 
 export default class NavComponent extends React.Component {
     render() {
-
         return (
-            <>
-                <Navbar bg="light">
-                  
-                    <Nav.Link href="/products">Products</Nav.Link>
-                    <Nav.Link href="/about">About</Nav.Link> 
-                    {(localStorage.getItem('userId') == null) ?
-                        (
-                            <>
-                                <Nav.Link href="/Login">Login</Nav.Link>
-                                <Nav.Link href="/Signup">SignUp</Nav.Link>
-                            </>
-                        ) : (
-                            <>
-                                <Nav.Link href="/Logout">Logout</Nav.Link>
-                            </>
-                        )
-                    }
-                </Navbar>
-            </>
+              <Navbar>
+                   
+                        <Nav>
+                            <Nav.Link href="/products">Products</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
+                        </Nav>
+                       
+                        <Nav>
+                            {(localStorage.getItem('userId') == null) ?
+                                (
+                                    <>
+                                        <Nav.Link href="/Login">Login</Nav.Link>
+                                        <Nav.Link href="/Signup">SignUp</Nav.Link>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Nav.Link href="/Logout">Logout</Nav.Link>
+                                    </>
+                                )
+                            }
+                        </Nav>
+                        
+            </Navbar>
         );
     }
 }
