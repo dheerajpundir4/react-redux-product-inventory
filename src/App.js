0488a5922debbd650508874bb07cb1c2540bde39
dheeeraj54321 <dheerajpundir@gmail.com>
2021-02-06 04:01:53 +0530

    XXX

--- src/App.js
+++ src/App.js
@@ -35,53 +35,52 @@ import SuccessComponent from './components/SuccessComponent'
 
 
 import NavComponent from './components/NavComponent'
-import RouterComponent from './components/RouterComponent'
 import { Card, Container } from 'react-bootstrap';
 
 import axios from 'axios';
 
 export default class App extends React.Component {
 
-  state={}
+  state = {}
 
-  componentDidMount(){  
+  componentDidMount() {
 
-    const config={
-      headers:{
-        Authorization:"Bearer"+localStorage.getItem('token')
+    const config = {
+      headers: {
+        Authorization: "Bearer" + localStorage.getItem('token')
       }
     }
 
-    const id=localStorage.getItem('id')
+    const id = localStorage.getItem('id')
 
-    axios.get('users/'+id,config).then(
-      res=>{
+    axios.get('users/' + id, config).then(
+      res => {
         console.log("=======success===========")
         console.log(config)
         console.log(res)
-        if(res.status=200){
+        if (res.status = 200) {
           console.log("not sure what to do")
 
-            this.setUser(res.data)
-            
-        
+          this.setUser(res.data)
+
+
           console.log("this.state.user")
           console.log(this.state.user)
         }
 
       },
-      err=>{
+      err => {
         console.log("=======error===========")
         console.log(err)
       }
     )
     console.log("++++++++++++++++++++++++")
-  } 
+  }
 
 
-  setUser=(us)=>{
+  setUser = (us) => {
     this.setState({
-      user:us
+      user: us
     })
   }
 
@@ -97,23 +96,23 @@ export default class App extends React.Component {
 
 
           <Router>
-                    <Switch>
-                        <Route path="/products" component={AllProductPage} />
-                        <Route path="/about" component={AboutPage} />
-                        <Route path="/topViewProduct" component={TopViewProduct} />
-                        <Route path="/addProduct" component={AddProductPage} />
-                        <Route path="/view/:id" component={ViewProductPage} />
-                        <Route path="/delete/:id" component={DeleteProductPage} />
-                        <Route path="/edit/:id" component={EditProductPage} />
-                        <Route exact path="/">
-                            <AllProductPage />
-                        </Route>
-                        <Route path="/Login" component={()=><Login setUser={this.setUser}/> } />
-                        <Route path="/Signup" component={Signup} />
-                        <Route path="/Logout" component={Logout} />
-                        <Route path="/success/:id" component={SuccessComponent} />
-                    </Switch>
-                </Router>
+            <Switch>
+              <Route path="/products" component={AllProductPage} />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/topViewProduct" component={TopViewProduct} />
+              <Route path="/addProduct" component={AddProductPage} />
+              <Route path="/view/:id" component={ViewProductPage} />
+              <Route path="/delete/:id" component={DeleteProductPage} />
+              <Route path="/edit/:id" component={EditProductPage} />
+              <Route exact path="/">
+                <AllProductPage />
+              </Route>
+              <Route path="/Login" component={() => <Login setUser={this.setUser} />} />
+              <Route path="/Signup" component={Signup} />
+              <Route path="/Logout" component={Logout} />
+              <Route path="/success/:id" component={SuccessComponent} />
+            </Switch>
+          </Router>
         </Container>
       </Container>
     );
