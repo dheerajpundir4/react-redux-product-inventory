488f3404b33106985f051c79cf703c24fb61f0a9
dheeeraj54321 <dheerajpundir@gmail.com>
2021-02-06 13:24:29 +0530

    filter part is done

--- src/pages/AllProduct/AllProductListPage.js
+++ src/pages/AllProduct/AllProductListPage.js
@@ -16,6 +16,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
 import axios from 'axios';
+import { filter } from 'json-server-auth';
 
 
 
@@ -25,9 +26,13 @@ class AllProductListPage extends React.Component {
 
   constructor() {
     super()
-    this.state = {
-      assignvalue:"naveen sing"
+    this.state = {     
+      value:'',
+      pro:'',
+      isFilter:false
     };
+
+    this.handleChange=this.handleChange.bind(this)
   }
     componentDidMount(){
       this.props.loadProduct();
@@ -37,18 +42,68 @@ class AllProductListPage extends React.Component {
   display(value) {
     console.log("Hello")
     console.log(value)
+   
     console.log(value.includes("productName"));
    
+  }
+
+  handleChange(event){
+    console.log("filer handlechange Product")   
+    console.log(event.target.value)
+    this.setState({value:event.target.value})
+    if(event.target.value==""){
+      this.setState({isfilter:false})
+      
+    }
+    else{
+     
+      console.log(event.target.value!="")
+     let product=this.props.products.filter(product=>{
+       console.log("checkig the value")
+       console.log(product.productName)
+       console.log(event.target.value)
+       console.log(product.productName==event.target.value)      
+       if(product.productName.includes(event.target.value))
+       return product
+     })
+     console.log("===product===")
+     console.log(product)
+     console.log(this.state.isfilter)
+     this.setState({pro:product,isfilter:true})
+    
+    }
   }
 
  
   
   render() {
-      console.log(this.state.assignvalue)
-    let productNodes = this.props.products.map(product =>
-      (
-        <SingleRow id={product.id} productName={product.productName} quantity={product.quantity} price={product.price} />
-      ));
+      
+    console.log("rendter")
+    console.log(this.state.pro)
+    console.log(this.props.products)
+    console.log(this.state.pro.length)
+    console.log(this.state.pro!=null)
+
+    console.log(this.state.isfilter)
+
+    let productNodes=""
+
+    if(this.state.isfilter){
+
+      productNodes = this.state.pro.map(product =>
+        (
+          <SingleRow id={product.id} productName={product.productName} quantity={product.quantity} price={product.price} />
+        ));
+    }
+    else{
+      productNodes = this.props.products.map(product =>
+        (
+          <SingleRow id={product.id} productName={product.productName} quantity={product.quantity} price={product.price} />
+        ));
+
+    }
+
+   
 
   
     return (
@@ -56,8 +111,12 @@ class AllProductListPage extends React.Component {
       <>
         {<CustomizationField cusValue={this.display} />}
         <Form inline>
-          <FormControl type="text" placeholder="Filter Product" className="mr-sm-2" />
-          <Button variant="outline-info">Filter</Button>
+          <Form.Control type="text"
+          onChange={this.handleChange}
+           placeholder="Filter" 
+           value={this.state.value}
+           className="mr-sm-2" />
+          
         </Form>
 
 
@@ -88,7 +147,7 @@ class AllProductListPage extends React.Component {
 }
 
 
-const mapStatetoProps = (state) => {
+const mapStatetoProps = (state) => {  
   return {
     products: state.products
   }
