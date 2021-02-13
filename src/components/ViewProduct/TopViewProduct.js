a8a48338c477475e289fc0d168badcf0017e898b
dheeeraj54321 <dheerajpundir@gmail.com>
2021-02-14 01:36:02 +0530

    XXX

--- src/components/ViewProduct/TopViewProduct.js
+++ src/components/ViewProduct/TopViewProduct.js
@@ -12,112 +12,108 @@ class TopViewProduct extends React.Component {
   constructor() {
     console.log("TopViewProduct Constructor")
     super()
-    this.state={
-      value:3,
-      label:"3"
+    this.state = {
+      VALUE: 3,
+      LABEL: "3"
     }
-   
+
   }
 
   //Comparer Function    
-GetSortOrder(prop) {    
-  return function(a, b) {    
-      if (a[prop] > b[prop]) {    
-          return -1;    
-      } else if (a[prop] < b[prop]) {    
-          return 1;    
-      }    
-      return 0;    
-  }    
-} 
+  GetSortOrder(prop) {
+    return function (a, b) {
+      if (a[prop] > b[prop]) {
+        return -1;
+      } else if (a[prop] < b[prop]) {
+        return 1;
+      }
+      return 0;
+    }
+  }
 
   render() {
 
     console.log("TopViewProduct render");
 
-    let prods= this.props.products
-    console.log(prods)
+    let products = this.props.products
 
-    let products=prods.sort(this.GetSortOrder("views"));
+    products.sort(this.GetSortOrder("views"));
 
-  products=products.slice(0,this.state.value)
+    products = products.slice(0, this.state.VALUE)
 
-    let p = products.map((product) => {
+    let productViewsArray = products.map((product) => {
       return product.views
     })
 
-    let q = products.map((product) => {
-
-      console.log(product.productName)
+    let productNameArray = products.map((product) => {
 
       return product.productName
     })
 
-    console.log("pppppppppppppppppppppp")
-    console.log(p)
 
-    const data = {
-      labels: q,
+
+    const topViewProductData = {
+      labels: productNameArray,
       datasets: [
         {
-          label: 'Total Views',
+          label: 'Top View Products',
           backgroundColor: 'rgba(255,99,132,0.2)',
           borderColor: 'rgba(255,99,132,1)',
           borderWidth: 1,
           hoverBackgroundColor: 'rgba(255,99,132,0.4)',
           hoverBorderColor: 'rgba(255,99,132,1)',
-          data: p
+          data: productViewsArray
         }
       ]
     };
-    let Top = "Top"
-/*    const options = [
-      "", "5", "10"
-    ];*/
 
-       const options = [
-        { value: 3, label: '3' },
-        { value: 5, label: '5' },
-        { value: 10, label: '10' },
+    const options = [
+      { value: 3, label: '3' },
+      { value: 5, label: '5' },
+      { value: 10, label: '10' },
     ];
-    const defaultOption = this.state.label
 
 
 
 
     return (
-      <>
-
-       
-         
-          <div className="w-auto h-auto d-inline-block">
-            Top
+      <> 
+       <div className="w-auto h-auto d-inline-block">
+          Top
             <Dropdown
-              options={options}
-              onChange={ (selected)=>{
-               
-                console.log(selected.value)
-                this.setState({value:selected.value, label:selected.label})
-              }                       
-              }
-              value={defaultOption}
-              placeholder="Select an option" />
+            options={options}
+            onChange={(selected) => {
+              this.setState({ VALUE: selected.value, LABEL: selected.label })
+            }
+            }
+            value={this.state.LABEL}
+           />
+        </div>  
 
 
 
-          </div>
-
-
-
-
+        <div className="w-auto h-auto ">
           <Bar
-            data={data}
+            data={topViewProductData}
             width={100}
             height={500}
             options={{
               maintainAspectRatio: false
             }}
           />
+        </div>
+
+        <div className="w-auto h-auto d-inline-block">
+          Top
+            <Dropdown
+            options={options}
+            onChange={(selected) => {
+              this.setState({ VALUE: selected.value, LABEL: selected.label })
+            }
+            }
+            value={this.state.LABEL}
+            />
+        </div>
       </>
     );
   }
@@ -125,14 +121,14 @@ GetSortOrder(prop) {
 
 const mapStatetoProps = (state) => {
   return {
-          products: state.products
+    products: state.products
   }
 }
 
 const mapDispatchtoProps = (dispatch) => {
   return {
 
-        }
+  }
 }
 
 export default connect(mapStatetoProps, mapDispatchtoProps)(TopViewProduct);
